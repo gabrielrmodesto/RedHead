424bd3509428d0cf2b7f224df1200e7cb7e16b94
GabrielRM <gabrielrmodesto5@gmail.com>
2018-12-14 20:24:48 -0200

    Parte de model com conexão e cliente

--- src/model/dao/ClienteDAO.java
+++ src/model/dao/ClienteDAO.java
@@ -5,10 +5,186 @@
  */
 package model.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.bean.Cliente;
+import model.db.ConnectionFactory;
+
 /**
  *
  * @author gabrielmodesto
  */
 public class ClienteDAO {
-    
+
+    public boolean create(Cliente c) {
+        Connection com = ConnectionFactory.getConnection();
+        PreparedStatement stmt = null;
+        boolean cadastro = false;
+        try {
+            stmt = com.prepareStatement("INSERT INTO CLIENTE (nomeCliente, cpf, endereco, dataNascimento, codProjeto, cep, bairro, cidade, estado) "
+                    + "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
+
+            stmt.setString(1, c.getNomeCliente());
+            stmt.setString(2, c.getCpf());
+            stmt.setString(3, c.getEndereco());
+            stmt.setString(4, c.getDataNascimento());
+            stmt.setInt(5, c.getCodProjeto());
+            stmt.setString(6, c.getCep());
+            stmt.setString(7, c.getBairro());
+            stmt.setString(8, c.getCidade());
+            stmt.setString(9, c.getEstado());
+
+            stmt.executeUpdate();
+            cadastro = true;
+        } catch (SQLException ex) {
+            Logger.getLogger(ClienteDAO.class.getName()).log(Level.SEVERE, null, ex);
+            cadastro = false;
+        } finally {
+            ConnectionFactory.closeConection(com, stmt);
+        }
+        return cadastro;
+    }
+
+    public ArrayList<Cliente> read() {
+        Connection com = ConnectionFactory.getConnection();
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+
+        ArrayList<Cliente> clientes = new ArrayList<>();
+        try {
+            stmt = com.prepareStatement("SELECT * FROM CLIENTE");
+            rs = stmt.executeQuery();
+
+            while (rs.next()) {
+                Cliente cliente = new Cliente();
+                cliente.setIdCliente(rs.getInt("idCliente"));
+                cliente.setNomeCliente(rs.getString("nomeCliente"));
+                cliente.setCpf(rs.getString("cpf"));
+                cliente.setEndereco(rs.getString("endereco"));
+                cliente.setDataNascimento(rs.getString("dataNascimento"));
+                cliente.setCodProjeto(rs.getInt("codProjeto"));
+                cliente.setCep(rs.getString("cep"));
+                cliente.setBairro(rs.getString("bairro"));
+                cliente.setCidade(rs.getString("cidade"));
+                cliente.setEstado(rs.getString("estado"));
+
+                clientes.add(cliente);
+
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(ClienteDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            ConnectionFactory.closeConection(com, stmt, rs);
+        }
+        return clientes;
+    }
+
+    public boolean update(Cliente c) {
+        Connection com = ConnectionFactory.getConnection();
+        PreparedStatement stmt = null;
+        boolean atualizado = false;
+        try {
+            stmt = com.prepareStatement("UPDATE CLIENTE SET nomeCliente = ?, cpf = ?, endereco = ?, "
+                    + "dataNascimento = ?, codProjeto = ?, cep = ?, bairro = ?, cidade = ?, estado = ? WHERE idCliente = ?");
+            stmt.setString(1, c.getNomeCliente());
+            stmt.setString(2, c.getCpf());
+            stmt.setString(3, c.getEndereco());
+            stmt.setString(4, c.getDataNascimento());
+            stmt.setInt(5, c.getCodProjeto());
+            stmt.setString(6, c.getCep());
+            stmt.setString(7, c.getBairro());
+            stmt.setString(8, c.getCidade());
+            stmt.setString(9, c.getEstado());
+            stmt.setInt(10, c.getIdCliente());
+
+            stmt.executeUpdate();
+            atualizado = true;
+        } catch (SQLException ex) {
+            Logger.getLogger(ClienteDAO.class.getName()).log(Level.SEVERE, null, ex);
+            atualizado = false;
+        } finally {
+            ConnectionFactory.closeConection(com, stmt);
+        }
+        return atualizado;
+    }
+
+    public boolean delete(Cliente c) {
+        Connection com = ConnectionFactory.getConnection();
+        PreparedStatement stmt = null;
+        boolean deletado = false;
+        try {
+            stmt = com.prepareStatement("DELETE FROM CLIENTE WHERE idCliente = ?");
+            stmt.setInt(1, c.getIdCliente());
+
+            stmt.executeUpdate();
+            deletado = true;
+
+        } catch (SQLException ex) {
+            Logger.getLogger(ClienteDAO.class.getName()).log(Level.SEVERE, null, ex);
+            deletado = false;
+        } finally {
+            ConnectionFactory.closeConection(com, stmt);
+        }
+        return deletado;
+    }
+
+    public ArrayList<Cliente> buscaCliente(String pesquisa) {
+        Connection com = ConnectionFactory.getConnection();
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+
+        ArrayList<Cliente> clientes = new ArrayList<>();
+        try {
+            stmt = com.prepareStatement("SELECT * FROM CLIENTE WHERE nomeCliente LIKE ? OR cpf LIKE ?");
+            stmt.setString(1, pesquisa + "%");
+            stmt.setString(2, pesquisa + "%");
+            rs = stmt.executeQuery();
+
+            while (rs.next()) {
+                Cliente cliente = new Cliente();
+                cliente.setIdCliente(rs.getInt("idCliente"));
+                cliente.setNomeCliente(rs.getString("nomeCliente"));
+                cliente.setCpf(rs.getString("cpf"));
+                cliente.setEndereco(rs.getString("endereco"));
+                cliente.setDataNascimento(rs.getString("dataNascimento"));
+                cliente.setCodProjeto(rs.getInt("codProjeto"));
+                cliente.setCep(rs.getString("cep"));
+                cliente.setBairro(rs.getString("bairro"));
+                cliente.setCidade(rs.getString("cidade"));
+                cliente.setEstado(rs.getString("estado"));
+
+                clientes.add(cliente);
+
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(ClienteDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            ConnectionFactory.closeConection(com, stmt, rs);
+        }
+        return clientes;
+    }
+
+    public int countCliente() {
+        Connection com = ConnectionFactory.getConnection();
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        int qtd = 0;
+
+        try {
+            stmt = com.prepareStatement("SELECT * FROM CLIENTE");
+            rs = stmt.executeQuery();
+            while (rs.next()) {
+                qtd++;
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return qtd;
+    }
+
 }
--- src/model/db/ConnectionFactory.java
+++ src/model/db/ConnectionFactory.java
@@ -5,10 +5,70 @@
  */
 package model.db;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author gabrielmodesto
  */
 public class ConnectionFactory {
+private static final String DRIVER = "com.mysql.jdbc.Driver";
+    private static final String URL = "jdbc:mysql://localhost:3306/REDHEAD";
+    private static final String USER = "root";
+    private static final String PASS = "root";
     
+     //METODO PARA CONECTAR O BANCO
+    public static Connection getConnection() {
+        try {
+            Class.forName(DRIVER);
+
+            return DriverManager.getConnection(URL, USER, PASS);
+
+        } catch (ClassNotFoundException | SQLException ex) {
+            throw new RuntimeException("erro na conexo: ", ex);
+        }
+    }
+    
+     public static void closeConection(Connection com) {
+
+        try {
+            if (com != null) {
+                com.close();
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(ConnectionFactory.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+     
+      public static void closeConection(Connection com, PreparedStatement stmt) {
+        closeConection(com);
+        try {
+            if(stmt != null){
+                stmt.close();
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(ConnectionFactory.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+      public static void closeConection(Connection com, PreparedStatement stmt, ResultSet rs) {
+        closeConection(com, stmt);
+        try {
+            if(rs != null){
+            rs.close();
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(ConnectionFactory.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+
 }
