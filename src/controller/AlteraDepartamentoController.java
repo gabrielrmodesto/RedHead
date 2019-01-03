ddfb06b8ce500024915d7870094478961e129e0b
GabrielRM <gabrielrmodesto5@gmail.com>
2019-01-03 12:38:39 -0200

    Funcionarios, departamento e projetos

--- src/controller/AlteraDepartamentoController.java
+++ src/controller/AlteraDepartamentoController.java
@@ -7,8 +7,19 @@ package controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXDatePicker;
+import com.jfoenix.controls.JFXDialog;
+import com.jfoenix.controls.JFXDialogLayout;
+import com.jfoenix.controls.JFXTextField;
 import javafx.fxml.Initializable;
-
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
+import model.bean.Departamento;
+import model.dao.DepartamentoDAO;
 /**
  * FXML Controller class
  *
@@ -16,12 +27,71 @@ import javafx.fxml.Initializable;
  */
 public class AlteraDepartamentoController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+    @FXML private StackPane stackAltera;
+    @FXML private Pane painelAlteraDepartamento;
+    @FXML private JFXButton btnLimpar;
+    @FXML private JFXButton btnEditar;
+    @FXML private JFXDatePicker inicio;
+    @FXML private JFXTextField nomeDepartamento;
+    @FXML private JFXTextField numeroFuncionarios;
+    @FXML private JFXTextField responsavel;
+    
+    Departamento departamento = new Departamento();
+    DepartamentoDAO departamentoDAO = new DepartamentoDAO();
+    
+    @FXML
+    public void editaCampos(ActionEvent event) {
+        departamento.setNomeDepartamento(nomeDepartamento.getText());
+        departamento.setNumeroFuncionario(Integer.parseInt(numeroFuncionarios.getText()));
+        departamento.setResponsavelDepartamento(responsavel.getText());
+        departamento.setInicioAtividades(inicio.getValue().toString());
+        
+        if((departamentoDAO.update(departamento)) && !camposVazios()){
+            JFXDialogLayout alerta = new JFXDialogLayout();
+            alerta.setBody(new Text("Edição realizada com Sucesso"));
+            alerta.setStyle("-fx-font-size: 16px; -fx-font-weight: bold;");
+            JFXDialog caixa = new JFXDialog(stackAltera, alerta, JFXDialog.DialogTransition.CENTER);
+            JFXButton sair = new JFXButton("OK");
+            sair.setOnAction(e -> {limpaCampos(null);caixa.close();});
+            alerta.setActions(sair);
+            caixa.show();
+        }
+        else{
+            JFXDialogLayout alerta = new JFXDialogLayout();
+            alerta.setBody(new Text("Os campos estão vazios"));
+            alerta.setStyle("-fx-font-size: 16px; -fx-font-weight: bold;");
+            JFXDialog caixa = new JFXDialog(stackAltera, alerta, JFXDialog.DialogTransition.CENTER);
+            JFXButton sair = new JFXButton("Não alterar nada");
+            sair.setOnAction(e -> {caixa.close();});
+            alerta.setActions(sair);
+            caixa.show();
+        }
+    }
+    
+    @FXML
+    public void limpaCampos(ActionEvent event) {
+        inicio.setValue(null);
+        nomeDepartamento.clear();
+        numeroFuncionarios.clear();
+        responsavel.clear();
+    }
+    private boolean camposVazios(){
+        boolean erro = false;
+        if(nomeDepartamento.getText().equals(""))
+                erro  = true;
+        if(responsavel.getText().equals(""))
+                erro = true;
+        if(numeroFuncionarios.getText().equals(""))
+                erro = true;
+        if(inicio.getValue() == null)
+                erro = true;
+
+        return erro;
+    }
+    
     @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+    public void initialize(URL location, ResourceBundle resources) {
+                
+    }
     
 }
--- src/controller/AlteraProjetoController.java
+++ src/controller/AlteraProjetoController.java
@@ -7,8 +7,19 @@ package controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.fxml.Initializable;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXDatePicker;
+import com.jfoenix.controls.JFXDialog;
+import com.jfoenix.controls.JFXDialogLayout;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
+import model.bean.Projeto;
+import model.dao.ProjetoDAO;
 /**
  * FXML Controller class
  *
@@ -16,12 +27,81 @@ import javafx.fxml.Initializable;
  */
 public class AlteraProjetoController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+    @FXML private StackPane stackEdita;
+    @FXML private JFXButton btnEditar;
+    @FXML private JFXButton btnLimpar;
+    @FXML private JFXTextField nomeProjeto;
+    @FXML private JFXTextField equipe;
+    @FXML private JFXDatePicker prazo;
+    @FXML private JFXTextField valor;
+    @FXML private JFXTextField cliente;
+    @FXML private JFXTextField cpfFuncionario;
+    @FXML private JFXTextField cpfCliente;
+    @FXML private JFXTextField codDepartamento;
+    
+    Projeto projeto = new Projeto();
+    ProjetoDAO projetoDAO = new ProjetoDAO();
+
+    @FXML
+    public void editarCampos(ActionEvent event) {
+        projeto.setNomeProjeto(nomeProjeto.getText());
+        projeto.setPrazo(prazo.getValue().toString());
+        projeto.setEquipe(Integer.parseInt(equipe.getText()));
+        projeto.setValor(Float.parseFloat(valor.getText()));
+        projeto.setCpfFuncionario(cpfFuncionario.getText());
+        projeto.setCodDepartamento(Integer.parseInt(codDepartamento.getText()));
+        projeto.setCpfCliente(cpfCliente.getText());
+        projeto.setNomeCliente(cliente.getText());
+        
+        if((projetoDAO.update(projeto)) && !camposVazios()){
+            JFXDialogLayout alerta = new JFXDialogLayout();
+            alerta.setBody(new Text("Edição Realizada com Sucesso"));
+            alerta.setStyle("-fx-font-size: 16px; -fx-font-weight: bold;");
+            JFXDialog caixa = new JFXDialog(stackEdita, alerta, JFXDialog.DialogTransition.CENTER);
+            JFXButton sair = new JFXButton("OK");
+            sair.setOnAction(e -> {limpaCampos(null);caixa.close();});
+            alerta.setActions(sair);			
+            caixa.show();	
+        }
+        else{
+            JFXDialogLayout alerta = new JFXDialogLayout();
+            alerta.setBody(new Text("Campos Vazios"));
+            alerta.setStyle("-fx-font-size: 16px; -fx-font-weight: bold;");
+            JFXDialog caixa = new JFXDialog(stackEdita, alerta, JFXDialog.DialogTransition.CENTER);
+            JFXButton sair = new JFXButton("Não alterar nada");
+            sair.setOnAction(e -> {caixa.close();});
+            alerta.setActions(sair);			
+            caixa.show();
+        }	
+    }
+
+    @FXML
+    public void limpaCampos(ActionEvent event) {
+    	nomeProjeto.clear();
+    	equipe.clear();
+    	prazo.setValue(null);
+    	valor.clear();
+    	cliente.clear();
+    }
+    private boolean camposVazios(){
+        boolean erro = false;
+        if(nomeProjeto.getText().equals(""))
+            erro = true;
+        if(equipe.getText().equals(""))
+            erro = true;
+        if(valor.getText().equals(""))
+            erro = true;
+        if(cliente.getText().equals(""))
+            erro = true;
+        if(prazo.getValue() == null)
+            erro = true;
+        
+        return erro;
+    }
     @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+    public void initialize(URL location, ResourceBundle resources) {
+            // TODO Auto-generated method stub
+
+    }
     
 }
