cdff021140bbbdafb7806a4905e22be50d34a62b
GabrielRM <gabrielrmodesto5@gmail.com>
2019-01-03 16:47:21 -0200

    Arquivo principal e bd e inserts

--- src/redheadmain/RedHeadMain.java
+++ src/redheadmain/RedHeadMain.java
@@ -6,11 +6,9 @@
 package redheadmain;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 /**
@@ -21,24 +19,16 @@ public class RedHeadMain extends Application {
     
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+       try{
+            Parent root = FXMLLoader.load(getClass().getResource("../view/Login.fxml"));
+            Scene scene = new Scene(root);
+            scene.getStylesheets().add(getClass().getResource("../view/style.css").toExternalForm());
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch(Exception e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        } 
     }
 
     /**
