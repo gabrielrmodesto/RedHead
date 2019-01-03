7f7b38d990242654e6367991e5ace18b34975840
GabrielRM <gabrielrmodesto5@gmail.com>
2019-01-03 15:24:46 -0200

    Controller relacionados ao usuario

--- src/controller/DashBoardController.java
+++ src/controller/DashBoardController.java
@@ -5,9 +5,23 @@
  */
 package controller;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.layout.Pane;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXDialog;
+import com.jfoenix.controls.JFXDialogLayout;
+import javafx.application.Platform;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
 
 /**
  * FXML Controller class
@@ -16,12 +30,145 @@ import javafx.fxml.Initializable;
  */
 public class DashBoardController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+    @FXML private JFXButton imageHome;
+    @FXML private AnchorPane anchorFixo;
+    @FXML private JFXButton btnDepartamento;
+    @FXML private JFXButton btnFuncionarios;
+    @FXML private JFXButton btnClientes;
+    @FXML private JFXButton btnProjetos;
+    @FXML private JFXButton btnSuporte;
+    @FXML private JFXButton botaoSair;
+    @FXML private StackPane stackDash;
+    @FXML private JFXButton botaoImagemUser;
+    @FXML private ImageView imagemUser;
+
+    @FXML
+    public void telaDepartamentos(ActionEvent event) throws IOException {
+        carregaTelaDepartamento();
+    }
+
+    @FXML
+    public void telaCientes(ActionEvent event) throws IOException {
+        carregaTelaClientes();
+    }
+
+    @FXML
+    public void telaFuncionarios(ActionEvent event) throws IOException {
+        carregaTelaFuncionarios();
+    }
+
+    @FXML
+    public void telaProjetos(ActionEvent event) throws IOException {
+        carregaTelaProjetos();
+    }
+
+    @FXML
+    public void telaSuporte(ActionEvent event) throws IOException {
+        carregaTelaSuporte();
+    }
+
+    @FXML
+    public void telaDash(ActionEvent event) throws IOException {
+        carregaDash();
+    }
+    @FXML
+    public void carregaTelaPerfil(ActionEvent event) throws IOException {
+        telaPerfil();
+    }
+    @FXML
+    public void sairPrograma(ActionEvent event) {
+        JFXDialogLayout alerta = new JFXDialogLayout();
+        alerta.setBody(new Text("Deseja realmente sair?"));
+        alerta.setStyle("-fx-font-size: 16px; -fx-font-weight: bold;");
+        JFXDialog caixa = new JFXDialog(stackDash, alerta, JFXDialog.DialogTransition.CENTER);
+        JFXButton sair = new JFXButton("SIM");
+        JFXButton voltar = new JFXButton("NÃO");
+        sair.setOnAction(e -> {caixa.close();Platform.exit();});
+        voltar.setOnAction(e -> {caixa.close();});
+        alerta.setActions(sair,voltar);			
+        caixa.show();	
+    }
+    private void carregaTelaDepartamento() throws IOException {
+        try {
+            Pane painelDepartamento = FXMLLoader.load(getClass().getResource("TelaPrincipalDepartamento.fxml"));
+            anchorFixo.getChildren().setAll(painelDepartamento);
+        } catch (Exception e) {
+            // TODO: handle exception
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private void carregaTelaClientes() throws IOException {
+        try {
+            Pane painelClientes = FXMLLoader.load(getClass().getResource("TelaPrincipalClientes.fxml"));
+            anchorFixo.getChildren().setAll(painelClientes);
+        } catch (Exception e) {
+            // TODO: handle exception
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private void carregaTelaFuncionarios() throws IOException {
+        try {
+            Pane painelFuncionarios = FXMLLoader.load(getClass().getResource("TelaPrincipalFuncionario.fxml"));
+            anchorFixo.getChildren().setAll(painelFuncionarios);
+        } catch (Exception e) {
+            // TODO: handle exception
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private void carregaTelaProjetos() throws IOException {
+        try {
+            Pane painelProjetos = FXMLLoader.load(getClass().getResource("TelaPrincipalProjetos.fxml"));
+            anchorFixo.getChildren().setAll(painelProjetos);
+        } catch (Exception e) {
+            // TODO: handle exception
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private void carregaTelaSuporte() throws IOException {
+        try {
+            Pane painelSuporte = FXMLLoader.load(getClass().getResource("TelaPrincipalSuporte.fxml"));
+            anchorFixo.getChildren().setAll(painelSuporte);
+        } catch (Exception e) {
+            // TODO: handle exception
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @FXML
+    public void carregaDash() throws IOException {
+        try {
+            Pane painelPrincipal = FXMLLoader.load(getClass().getResource("PaneGrafico.fxml"));
+            anchorFixo.getChildren().setAll(painelPrincipal);
+        } catch (Exception e) {
+            // TODO: handle exception
+            System.out.println(e.getMessage());
+        }
+    }
+
     @Override
-    public void initialize(URL url, ResourceBundle rb) {
+    public void initialize(URL url, ResourceBundle resources) {
         // TODO
-    }    
+        try {
+            carregaDash();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    private void telaPerfil() throws IOException{
+        try {
+            Pane painelPerfil = FXMLLoader.load(getClass().getResource("perfilGerente.fxml"));
+            anchorFixo.getChildren().setAll(painelPerfil);
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println(e.getMessage());
+        }
+    }
     
 }
--- src/controller/EscolherLoginController.java
+++ src/controller/EscolherLoginController.java
@@ -5,9 +5,22 @@
  */
 package controller;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXDialog;
+import com.jfoenix.controls.JFXDialogLayout;
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
+import model.bean.Usuario;
+import model.dao.UsuarioDAO;
 
 /**
  * FXML Controller class
@@ -16,12 +29,59 @@ import javafx.fxml.Initializable;
  */
 public class EscolherLoginController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+    @FXML private StackPane painelLogin;
+    @FXML private JFXButton btnEnviar;
+    @FXML private JFXButton btnLmpar;
+    @FXML private JFXPasswordField campoSenha;
+    @FXML private JFXPasswordField campoConfirmaSenha;
+    @FXML private JFXTextField campoLogin;
+    Usuario user = new Usuario();
+    UsuarioDAO usuario = new UsuarioDAO();
+    
+    @FXML
+    public void confirmaLogin(ActionEvent event){
+        user.setLogin(campoLogin.getText());
+        user.setSenha(campoSenha.getText());
+        if(usuario.create(user)){
+            JFXDialogLayout alerta = new JFXDialogLayout();
+            alerta.setBody(new Text("Cadastro Realizado com Sucesso"));
+            alerta.setStyle("-fx-font-size: 16px; -fx-font-weight: bold;");
+            JFXDialog caixa = new JFXDialog(painelLogin, alerta, JFXDialog.DialogTransition.CENTER);
+            JFXButton sair = new JFXButton("OK");
+            sair.setOnAction(e -> {limpaDados(null);caixa.close();voltaCadastro();});
+            alerta.setActions(sair);
+            caixa.show();
+            }
+        else{
+            JFXDialogLayout alerta = new JFXDialogLayout();
+            alerta.setBody(new Text("Cadastro Não Realizado"));
+            alerta.setStyle("-fx-font-size: 16px; -fx-font-weight: bold;");
+            JFXDialog caixa = new JFXDialog(painelLogin, alerta, JFXDialog.DialogTransition.CENTER);
+            JFXButton sair = new JFXButton("Tente Novamente");
+            sair.setOnAction(e -> {caixa.close();});
+            alerta.setActions(sair);
+            caixa.show();
+        }
+    }
+    
+    private void voltaCadastro() {
+        try {
+            Pane painelCadastro = FXMLLoader.load(getClass().getResource("CadastroFuncionario.fxml"));
+            painelLogin.getChildren().setAll(painelCadastro);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        
+    }
+    @FXML
+    public void limpaDados(ActionEvent event) {
+    	campoConfirmaSenha.clear();
+    	campoSenha.clear();
+    	campoLogin.clear();
+    }
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+    }   
     
 }
